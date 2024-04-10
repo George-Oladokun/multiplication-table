149c77a8e8e843e68c62ace66f5cd510841f5d42
George-Oladokun <oladokungeorge8@gmail.com>
2024-04-10 13:13:01 +0100

    multiplication table

---
+++ script.js
@@ -0,0 +1,12 @@
+function mul() {
+    disp.innerHTML = ""
+    for (i = 1; i <= 12; i++) {
+        var score = inPut.value
+        if (score == 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, Number)
+            var total = score * i
+        disp.innerHTML += `<table><tr><td> <h3>${score}*${i}=${total} </h3></td></tr></table>`
+        alert("Am working. If you input any number you want to input it will list the multiplication table.")
+    } if (score == "") {
+        alert("Abeg input something jo 🙏🙏")
+    }
+}     
